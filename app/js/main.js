05e37e8bc4e3f4fd5bd5588426a9c70097f7493f
galinayakibchuk <galina.yakibchuk@gmail.com>
2025-10-08 11:54:44 +0200

    added page 404

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.accessories__slider', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.arrow-next',
+    prevEl: '.arrow-prev',
+  },
+
+  
+});
