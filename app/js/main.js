fdd4aacdf3159c1d913d44da96f3ef045bde3982
galinayakibchuk <galina.yakibchuk@gmail.com>
2025-10-24 17:21:39 +0200

    added page catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
+const viewList = document.querySelector('.view-mode__btn--list');
+const viewGrid = document.querySelector('.view-mode__btn--grid');
+const viewContainer = document.querySelector('.view-mode__container');
+
+viewGrid.addEventListener('click', ()=>{
+  viewContainer.classList.add('view-mode__container--grid');
+  viewContainer.classList.remove('view-mode__container--list');
+});
+
+viewList.addEventListener('click', () => {
+  viewContainer.classList.add('view-mode__container--list');
+  viewContainer.classList.remove('view-mode__container--grid');
+});
+
+
+
+
+
 const swiper = new Swiper('.accessories__slider', {
   slidesPerView: 3,
   spaceBetween: 40,
@@ -8,5 +26,56 @@ const swiper = new Swiper('.accessories__slider', {
     prevEl: '.arrow-prev',
   },
 
+
+});
+
+const swiperReviews = new Swiper('.reviews-slider', {
+  slidesPerView: 12,
+  spaceBetween: 16,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider-next',
+    prevEl: '.reviews-slider-prev',
+  },
+  pagination: {
+    el: ".reviews-slider-pagination",
+    type: "fraction",
+  },
+
+});
+
+
+const rangeSlider = document.querySelector('.range__slider');
+const inputMin = document.querySelector('.range__min');
+const inputMax = document.querySelector('.range__max');
+
+noUiSlider.create(rangeSlider, {
+  start: [300, 3000],
+  step: 100,
+  range: {
+    'min': 300,
+    'max': 3000
+  },
+  format: {
+    to: value => Math.round(value),
+    from: value => Number(value)
+  }
+});
+
+rangeSlider.noUiSlider.on('update', (values, handle) => {
+  if (handle === 0) {
+    inputMin.value = values[0];
+  } else {
+    inputMax.value = values[1];
+  }
   
 });
+
+inputMin.addEventListener('change', () =>{
+  rangeSlider.noUiSlider.set([inputMin.value, null])
+})
+
+inputMax.addEventListener('change', () => {
+  rangeSlider.noUiSlider.set([null, inputMax.value])
+})
