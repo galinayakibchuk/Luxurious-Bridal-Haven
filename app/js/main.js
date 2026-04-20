cd961b90e9160344b0f26c1970a7877cbe4c520b
galinayakibchuk <galina.yakibchuk@gmail.com>
2026-04-20 17:10:43 +0200

    added responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,124 @@
+const openBtn = document.querySelector('.open-modal');
+const closeBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+
+
+function openModal() {
+  modal.setAttribute('aria-hidden', 'false')
+  document.body.classList.add('scroll-lock')
+}
+
+function closeModal() {
+  modal.setAttribute('aria-hidden', 'true')
+  document.body.classList.remove('scroll-lock')
+}
+
+if (modalOverlay) {
+  modalOverlay.addEventListener('click', closeModal) 
+}
+
+
+openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modal.getAttribute('aria-hidden') === 'false') {
+    closeModal();
+  }
+});
+
+
+
+const openBtnPopup = document.querySelector('.open-popup');
+const closeBtnPopup = document.querySelector('.close-popup');
+const popup = document.querySelector('.popup');
+const popupOverlay = document.querySelector('.popup__overlay');
+
+
+function openPopup() {
+  popup.setAttribute('aria-hidden', 'false')
+  document.body.classList.add('scroll-lock')
+}
+
+function closePopup() {
+  popup.setAttribute('aria-hidden', 'true')
+  document.body.classList.remove('scroll-lock')
+}
+
+if (popupOverlay) {
+  modalOverlay.addEventListener('click', closePopup)
+}
+
+
+openBtnPopup.addEventListener('click', openPopup);
+closeBtnPopup.addEventListener('click', closePopup);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && popup.getAttribute('aria-hidden') === 'false') {
+    closePopup();
+  }
+});
+
+
+
+
+const breakpoint = window.matchMedia('(min-width: 600px)');
+let sliderMobile = null;
+
+function initSwiper() {
+  sliderMobile = new Swiper('.slider-mobile', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.product__arrow-next',
+      prevEl: '.product__arrow-prev',
+    }
+  })
+}
+
+function destroySwiper() {
+  if (sliderMobile) {
+    sliderMobile.destroy(true, true);
+    sliderMobile = null;
+  }
+}
+
+function handleBreakpointChange(e) {
+  if (e.matches) {
+    destroySwiper()
+  } else {
+    if (!sliderMobile) initSwiper()
+  }
+}
+
+handleBreakpointChange(breakpoint);
+
+breakpoint.addEventListener('change', handleBreakpointChange);
+
+
+
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active')
+})
+
+
 const viewList = document.querySelector('.view-mode__btn--list');
 const viewGrid = document.querySelector('.view-mode__btn--grid');
 const viewContainer = document.querySelector('.view-mode__container');
 
-viewGrid.addEventListener('click', ()=>{
+
+
+viewGrid?.addEventListener('click', () => {
   viewContainer.classList.add('view-mode__container--grid');
   viewContainer.classList.remove('view-mode__container--list');
 });
 
-viewList.addEventListener('click', () => {
+viewList?.addEventListener('click', () => {
   viewContainer.classList.add('view-mode__container--list');
   viewContainer.classList.remove('view-mode__container--grid');
 });
@@ -25,6 +136,17 @@ const swiper = new Swiper('.accessories__slider', {
     nextEl: '.arrow-next',
     prevEl: '.arrow-prev',
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 3,
+    },
+  },
 
 
 });
@@ -41,6 +163,20 @@ const swiperReviews = new Swiper('.reviews-slider', {
   pagination: {
     el: ".reviews-slider-pagination",
     type: "fraction",
+  },
+  breakpoints: {
+    360: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 8,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 12,
+      spaceBetween: 16,
+    },
   },
 
 });
@@ -69,10 +205,10 @@ rangeSlider.noUiSlider.on('update', (values, handle) => {
   } else {
     inputMax.value = values[1];
   }
-  
+
 });
 
-inputMin.addEventListener('change', () =>{
+inputMin.addEventListener('change', () => {
   rangeSlider.noUiSlider.set([inputMin.value, null])
 })
 
